d14da8ea1292b4092e423b05e9b2fc9383885c95
Ksusha Rusinova <rusinovakseniaolegovna@yandex.ru>
2024-11-04 12:55:45 +0400

    My unit tests

---
+++ task_3.java
@@ -0,0 +1,36 @@
+@RunWith(Parameterized.class) // Напиши аннотацию для параметризованных 
+тестов
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	this.age = age;
+    this.result = result;
+    // Инициализируй поля класса в конструкторе
+  }
+
+  @Parameterized.Parameters // Пометь метод аннотацией для параметров
+  public static Object[][] getTextData() {
+	return new Object[][] {
+        {19, true},
+        {17, false},
+        {18, true},
+        {21, true},
+        // Заполни массив тестовыми данными и ожидаемым результатом
+    };
+  }
+
+  @Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+    // Передай сюда возраст пользователя
+    boolean isAdult = program.checkIsAdult(age);
+	// Сравни полученный и ожидаемый результаты, не забудь про 
+сообщение об ошибке
+    assertEquals("Если пользователю из россии больше 18 или 18 лет, 
+выводится true. Если меньше 18 лет, выводится false. Если пользователю из 
+америки 21 год, выводится true." ,result, isAdult);
+	}
+}
